796bb332650f997cae7600e0687a0c5aa6e5f445
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2023-03-14 12:21:16 +0000

    v1.0.0

--- src/main/java/dev/teamcitrus/arcane/block/GlassJarBlock.java
+++ src/main/java/dev/teamcitrus/arcane/block/GlassJarBlock.java
@@ -2,21 +2,34 @@ package dev.teamcitrus.arcane.block;
 
 import dev.teamcitrus.arcane.blockentity.GlassJarBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GlassJarBlock extends Block implements EntityBlock {
+    public static final BooleanProperty HANGING = BlockStateProperties.HANGING;
+
     public GlassJarBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(HANGING, false));
     }
 
     @Nullable
@@ -45,5 +58,42 @@ public class GlassJarBlock extends Block implements EntityBlock {
         }
 
         super.onRemove(oldState, level, pos, newState, p_60519_);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        for(Direction direction : context.getNearestLookingDirections()) {
+            if (direction.getAxis() == Direction.Axis.Y) {
+                this.defaultBlockState().setValue(HANGING, direction == Direction.UP);
+            }
+        }
+
+        return super.getStateForPlacement(context);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(HANGING);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        Direction direction = getConnectedDirection(state).getOpposite();
+        return Block.canSupportCenter(level, pos.relative(direction), direction.getOpposite());
+    }
+
+    protected static Direction getConnectedDirection(BlockState state) {
+        return state.getValue(HANGING) ? Direction.DOWN : Direction.UP;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState oldState, Direction direction, BlockState newState, LevelAccessor level, BlockPos newPos, BlockPos oldPos) {
+        return getConnectedDirection(oldState).getOpposite() == direction && !oldState.canSurvive(level, newPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(oldState, direction, newState, level, newPos, oldPos);
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState state, BlockGetter getter, BlockPos pos, PathComputationType path) {
+        return false;
     }
 }
